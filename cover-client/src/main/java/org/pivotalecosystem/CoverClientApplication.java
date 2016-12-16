63c5230ad6bd78662facdea515ea8a3cf6bcce3a
Joshua McKenty <jmckenty@pivotal.io>
2016-12-15 22:07:46 -0800

    Fall back to embedded DB for tests.

--- cover-client/src/main/java/org/pivotalecosystem/CoverClientApplication.java
+++ cover-client/src/main/java/org/pivotalecosystem/CoverClientApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CoverClientApplication {
 
     @Bean
--- frontend/src/main/java/org/pivotalecosystem/FrontendApplication.java
+++ frontend/src/main/java/org/pivotalecosystem/FrontendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import java.net.URI;
 
 @EnableCircuitBreaker
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FrontendApplication {
     @Bean
     @LoadBalanced
